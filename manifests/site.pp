5c0c422a3092e0e9eda39e0f266804382ed8a863
Diego Malicia <dmalicia@gmail.com>
2020-09-23 05:21:03 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,36 +1,21 @@
 node default { } 
 node /^monitoring(?:-\d+)?/ { 
     include promgrafana 
-file {'/etc/motd':                                          
-  ensure  => 'present',                                          
-  mode    => '0644',                                  
-  content => 'dmlc.pw node under puppet.
-      work in progress
-###############################################
-###############################################
-###############################################
-###############################################', 
-
-}
 }
 
 node /^frontend(?:-\d+)?/ { 
-
-file {'/etc/motd':                                          
-  ensure  => 'present',                                          
-  mode    => '0644',                                  
-  content => 'dmlc.pw node under puppet.
-      work in progress
-###############################################
-###############################################
-###############################################
-###############################################
-  ',           
-
-}
   docker::run { 'frontend':
      image     => 'dmalicia/nodejs-simple',
      ports     => ['80:3000'],
      expose    => ['80'],
   }
 }
+
+node /^cassandra(?:-\d+)?/ { 
+   include cassandra
+}
+
+node /^backend(?:-\d+)?/ { 
+   include backend
+}
+
